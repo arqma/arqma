72e0a484b1b705caa8b5551774f8cea9786abb83
Michal vel m@lbit <michal@arqma.com>
2018-08-14 21:22:41 +0100

    file locking fix

--- src/common/util.h
+++ src/common/util.h
@@ -91,6 +91,20 @@ namespace tools
     const std::string& filename() const noexcept { return m_filename; }
   };
 
+  class file_locker
+  {
+  public:
+    file_locker(const std::string &filename);
+    ~file_locker();
+    bool locked() const;
+  private:
+ #ifdef WIN32
+    HANDLE m_fd;
+ #else
+    int m_fd;
+ #endif
+  };
+  
   /*! \brief Returns the default data directory.
    *
    * \details Windows < Vista: C:\\Documents and Settings\\Username\\Application Data\\CRYPTONOTE_NAME
@@ -214,7 +228,9 @@ namespace tools
   bool sha256sum(const uint8_t *data, size_t len, crypto::hash &hash);
   bool sha256sum(const std::string &filename, crypto::hash &hash);
 
+  bool is_hdd(const char *path);
+  
   boost::optional<std::pair<uint32_t, uint32_t>> parse_subaddress_lookahead(const std::string& str);
 
-  bool is_hdd(const char *path);
+  std::string glob_to_regex(const std::string &val);
 }
