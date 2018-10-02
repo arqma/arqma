811aac8a30d07125fabbf0bd029db4acecb58a14
Michal vel m@lbit <michal@arqma.com>
2018-10-02 01:58:53 +0100

    android fix

--- build-all-arch.sh
+++ build-all-arch.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 #
-#  -D BOOST_ROOT=/opt/android/boost_1_67_0
+#  -D BOOST_ROOT=/opt/android/boost_1_58_0
 
- set -e
- orig_path=$PATH
+set -e
+
+orig_path=$PATH
 base_dir=`pwd`
- build_type=release # or debug
- archs=(arm arm64 x86 x86_64)
+
+build_type=release # or debug
+
+archs=(arm arm64 x86 x86_64)
 #archs=(x86)
- for arch in ${archs[@]}; do
+
+for arch in ${archs[@]}; do
 	ldflags=""
     case ${arch} in
-
-"arm")
+        "arm")
 			target_host=arm-linux-androideabi
 			ldflags="-march=armv7-a -Wl,--fix-cortex-a8"
 			xarch=armv7-a
@@ -36,20 +39,26 @@ base_dir=`pwd`
 			exit 16
             ;;
     esac
- 	OUTPUT_DIR=$base_dir/build/$build_type.$arch
- 	mkdir -p $OUTPUT_DIR
+
+	OUTPUT_DIR=$base_dir/build/$build_type.$arch
+
+	mkdir -p $OUTPUT_DIR
 	cd $OUTPUT_DIR
- 	PATH=/opt/android/tool/$arch/$target_host/bin:/opt/android/tool/$arch/bin:$PATH CC=clang CXX=clang++ cmake -D BUILD_GUI_DEPS=1 -D BUILD_TESTS=OFF -D ARCH="$xarch" -D STATIC=ON -D BUILD_64=$sixtyfour -D CMAKE_BUILD_TYPE=$build_type -D ANDROID=true -D BUILD_TAG="android" -D BOOST_ROOT=/opt/android/build/boost/$arch -D BOOST_LIBRARYDIR=/opt/android/build/boost/$arch/lib -D OPENSSL_INCLUDE_DIR=/opt/android/build/openssl/include OPENSSL_ROOT_DIR=/opt/android/build/openssl/$arch -D OPENSSL_CRYPTO_LIBRARY=/opt/android/build/openssl/$arch/lib/libcrypto.so -D OPENSSL_SSL_LIBRARY=/opt/android/build/openssl/$arch/lib/libssl.so -D CMAKE_POSITION_INDEPENDENT_CODE:BOOL=true ../..
-	make -j2 wallet_api
+
+	PATH=/opt/android/tool/$arch/$target_host/bin:/opt/android/tool/$arch/bin:$PATH CC=clang CXX=clang++ cmake -D BUILD_GUI_DEPS=1 -D BUILD_TESTS=OFF -D ARCH="$xarch" -D STATIC=ON -D BUILD_64=$sixtyfour -D CMAKE_BUILD_TYPE=$build_type -D ANDROID=true -D BUILD_TAG="android" -D BOOST_ROOT=/opt/android/build/boost/$arch -D BOOST_LIBRARYDIR=/opt/android/build/boost/$arch/lib -D OPENSSL_INCLUDE_DIR=/opt/android/build/openssl/include OPENSSL_ROOT_DIR=/opt/android/build/openssl/$arch -D OPENSSL_CRYPTO_LIBRARY=/opt/android/build/openssl/$arch/lib/libcrypto.so -D OPENSSL_SSL_LIBRARY=/opt/android/build/openssl/$arch/lib/libssl.so -D CMAKE_POSITION_INDEPENDENT_CODE:BOOL=true ../..
+	make -j4 wallet_api
 	find . -path ./lib -prune -o -name '*.a' -exec cp '{}' lib \;
-     TARGET_LIB_DIR=/opt/android/build/arqma/$arch/lib
+
+    TARGET_LIB_DIR=/opt/android/build/arqma/$arch/lib
     rm -rf $TARGET_LIB_DIR
     mkdir -p $TARGET_LIB_DIR
     cp $OUTPUT_DIR/lib/*.a $TARGET_LIB_DIR
-     TARGET_INC_DIR=/opt/android/build/arqma/include
+
+    TARGET_INC_DIR=/opt/android/build/arqma/include
     rm -rf $TARGET_INC_DIR
     mkdir -p $TARGET_INC_DIR
 	cp -a ../../src/wallet/api/wallet2_api.h $TARGET_INC_DIR
- 	cd $base_dir
+
+	cd $base_dir
 done
 exit 0
