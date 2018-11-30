0ec5d7974028e0fe09983baf4e37bc46d373d174
ArqTras <33489188+ArqTras@users.noreply.github.com>
2018-11-30 10:04:39 +0100

    Correct one

---
+++ src/version.h
@@ -0,0 +1,6 @@
+#pragma once
+
+extern const char* const ARQMA_VERSION_TAG;
+extern const char* const ARQMA_VERSION;
+extern const char* const ARQMA_RELEASE_NAME;
+extern const char* const ARQMA_VERSION_FULL;
