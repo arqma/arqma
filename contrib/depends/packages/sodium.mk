861592fc319db594ca459a08205ea20cd1264b09
Michal vel m@lbit <malbit@users.noreply.github.com>
2019-02-06 12:56:59 +0000

    libsodium download link fix

--- contrib/depends/packages/sodium.mk
+++ contrib/depends/packages/sodium.mk
@@ -1,6 +1,6 @@
 package=sodium
 $(package)_version=1.0.16
-$(package)_download_path=https://download.libsodium.org/libsodium/releases/old/
+$(package)_download_path=https://download.libsodium.org/libsodium/releases/
 $(package)_file_name=libsodium-$($(package)_version).tar.gz
 $(package)_sha256_hash=eeadc7e1e1bcef09680fb4837d448fbdf57224978f865ac1c16745868fbd0533
 $(package)_patches=fix-whitespace.patch
