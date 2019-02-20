a67894734500b217611a983b6fc93a397ccc03c9
Michal vel m@lbit <malbit@users.noreply.github.com>
2019-02-20 14:05:00 +0000

    get_output_key method const

--- src/blockchain_db/blockchain_db.h
+++ src/blockchain_db/blockchain_db.h
@@ -1258,7 +1258,7 @@ public:
    *
    * @return the requested output data
    */
-  virtual output_data_t get_output_key(const uint64_t& amount, const uint64_t& index) = 0;
+  virtual output_data_t get_output_key(const uint64_t& amount, const uint64_t& index) const = 0;
 
   /**
    * @brief get some of an output's data
@@ -1316,7 +1316,7 @@ public:
    * @param offsets a list of amount-specific output indices
    * @param outputs return-by-reference a list of outputs' metadata
    */
-  virtual void get_output_key(const uint64_t &amount, const std::vector<uint64_t> &offsets, std::vector<output_data_t> &outputs, bool allow_partial = false) = 0;
+  virtual void get_output_key(const uint64_t &amount, const std::vector<uint64_t> &offsets, std::vector<output_data_t> &outputs, bool allow_partial = false) const = 0;
 
   /*
    * FIXME: Need to check with git blame and ask what this does to
