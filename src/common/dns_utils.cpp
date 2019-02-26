ddf700b5c99537f1f19e586ac198a78c117216cc
Michal vel m@lbit <malbit@users.noreply.github.com>
2019-02-26 15:27:23 +0000

    leftovers cleaning

--- src/common/dns_utils.cpp
+++ src/common/dns_utils.cpp
@@ -285,7 +285,7 @@ DNSResolver::DNSResolver() : m_data(new DNSResolverData())
     // should be a valid DNSSEC record, and switch to known good
 	// DNSSEC resolvers if verification fails
 	bool available, valid;
-	static const char *probe_hostname = "updates.moneropulse.org";
+	static const char *probe_hostname = "";
 	auto records = get_txt_record(probe_hostname, available, valid);
 	if (!valid)
 	{
