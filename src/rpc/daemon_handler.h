ecfbe75b6c4c28f9c4085b46cf85c71922030fa0
Michal vel m@lbit <malbit@users.noreply.github.com>
2019-03-01 10:18:58 +0000

    Allow submitting tx as hex blob over ZMQ

--- src/rpc/daemon_handler.h
+++ src/rpc/daemon_handler.h
@@ -67,6 +67,8 @@ class DaemonHandler : public RpcHandler
     void handle(const GetTxGlobalOutputIndices::Request& req, GetTxGlobalOutputIndices::Response& res);
 
     void handle(const SendRawTx::Request& req, SendRawTx::Response& res);
+    
+    void handle(const SendRawTxHex::Request& req, SendRawTxHex::Response& res);
 
     void handle(const StartMining::Request& req, StartMining::Response& res);
 
@@ -133,6 +135,8 @@ class DaemonHandler : public RpcHandler
   private:
 
     bool getBlockHeaderByHash(const crypto::hash& hash_in, cryptonote::rpc::BlockHeaderResponse& response);
+    
+    void handleTxBlob(const std::string& tx_blob, bool relay, SendRawTx::Response& res);
 
     cryptonote::core& m_core;
     t_p2p& m_p2p;
