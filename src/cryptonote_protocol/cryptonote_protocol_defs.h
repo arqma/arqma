4eb645697db6f69e29fe7c18d46bdc57bc11b76a
Michal vel m@lbit <malbit@users.noreply.github.com>
2019-03-14 17:58:18 +0000

    public-node[WIP]

--- src/cryptonote_protocol/cryptonote_protocol_defs.h
+++ src/cryptonote_protocol/cryptonote_protocol_defs.h
@@ -55,6 +55,7 @@ namespace cryptonote
     std::string host;
     std::string ip;
     std::string port;
+    uint16_t rpc_port;
 
     std::string peer_id;
 
@@ -90,6 +91,7 @@ namespace cryptonote
       KV_SERIALIZE(host)
       KV_SERIALIZE(ip)
       KV_SERIALIZE(port)
+      KV_SERIALIZE(rpc_port)
       KV_SERIALIZE(peer_id)
       KV_SERIALIZE(recv_count)
       KV_SERIALIZE(recv_idle_time)
