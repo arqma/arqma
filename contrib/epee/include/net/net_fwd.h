808bd6444bd34a746f866fbf8a951f4730985fd9
m@lbit <malbit@arqma.com>
2019-05-23 06:11:56 +0100

    ssl common fixes

--- contrib/epee/include/net/net_fwd.h
+++ contrib/epee/include/net/net_fwd.h
@@ -1,16 +1,4 @@
-/**
-@file
-@details
-
-
-Passing RPC commands:
-
-@image html images/other/runtime-commands.png
-
-*/
-
-// Copyright (c) 2018-2019, The Arqma Network
-// Copyright (c) 2014-2018, The Monero Project
+// Copyright (c) 2019, The Monero Project
 //
 // All rights reserved.
 //
@@ -40,43 +28,11 @@ Passing RPC commands:
 
 #pragma once
 
-#include <boost/optional/optional_fwd.hpp>
-#include "common/common_fwd.h"
-#include "console_handler.h"
-#include "daemon/command_parser_executor.h"
-#include "net/net_fwd.h"
-
-namespace daemonize {
-
-class t_command_server {
-private:
-  t_command_parser_executor m_parser;
-  epee::console_handlers_binder m_command_lookup;
-  bool m_is_rpc;
-
-public:
-  t_command_server(
-      uint32_t ip
-    , uint16_t port
-    , const boost::optional<tools::login>& login
-    , const epee::net_utils::ssl_options_t& ssl_options
-    , bool is_rpc = true
-    , cryptonote::core_rpc_server* rpc_server = NULL
-    );
-
-  bool process_command_str(const std::string& cmd);
-
-  bool process_command_vec(const std::vector<std::string>& cmd);
-
-  bool start_handling(std::function<void(void)> exit_handler = NULL);
-
-  void stop_handling();
-
-private:
-  bool help(const std::vector<std::string>& args);
-
-  std::string get_commands_str();
-  std::string get_command_usage(const std::vector<std::string> &args);
-};
-
-} // namespace daemonize
+namespace epee
+{
+  namespace net_utils
+  {
+    struct ssl_authentication_t;
+    class ssl_options_t;
+  }
+}
