7f61599d931972ab1368a6d65800ec54210272f1
Michal vel m@lbit <michal@arqma.com>
2019-06-11 09:25:30 +0100

    make straus cached mode thread safe

--- src/ringct/multiexp.h
+++ src/ringct/multiexp.h
@@ -37,6 +37,7 @@
 #include <vector>
 #include "crypto/crypto.h"
 #include "rctTypes.h"
+#include "misc_log_ex.h"
 
 namespace rct
 {
@@ -53,9 +54,13 @@ struct MultiexpData {
   }
 };
 
+struct straus_cached_data;
+
 rct::key bos_coster_heap_conv(std::vector<MultiexpData> data);
 rct::key bos_coster_heap_conv_robust(std::vector<MultiexpData> data);
-rct::key straus(const std::vector<MultiexpData> &data, bool HiGi = false);
+std::shared_ptr<straus_cached_data> straus_init_cache(const std::vector<MultiexpData> &data);
+size_t straus_get_cache_size(const std::shared_ptr<straus_cached_data> &cache);
+rct::key straus(const std::vector<MultiexpData> &data, const std::shared_ptr<straus_cached_data> &cache = NULL);
 
 }
 
