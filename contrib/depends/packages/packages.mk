cc86de112e8db905be57c12c32bdfe5c2942afed
Michal vel m@lbit <michal@arqma.com>
2019-06-30 03:52:26 +0100

    depends: Fix readline compilation

--- contrib/depends/packages/packages.mk
+++ contrib/depends/packages/packages.mk
@@ -1,16 +1,12 @@
-packages:=boost openssl zeromq cppzmq expat ldns cppzmq readline libiconv hidapi protobuf libusb
+packages:=boost openssl zeromq cppzmq expat ldns cppzmq libiconv hidapi protobuf libusb
 native_packages := native_ccache native_protobuf
 
 darwin_native_packages = native_biplist native_ds_store native_mac_alias
-darwin_packages = sodium-darwin unbound
+darwin_packages = sodium-darwin unbound ncurses readline
 
-linux_packages = eudev unbound
+linux_packages = eudev unbound ncurses readline unwind sodium
 qt_packages = qt
 
-ifeq ($(host_os),linux)
-packages += unwind
-packages += sodium
-endif
 ifeq ($(host_os),mingw32)
 packages += icu4c
 packages += sodium
