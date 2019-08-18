fb57cdb864a51cbe02e97bf0bf52032cc1ce698a
Michal m@lbit Malicki <michal@arqma.com>
2019-08-18 12:24:51 +0100

    depends update

--- contrib/depends/packages/packages.mk
+++ contrib/depends/packages/packages.mk
@@ -1,10 +1,10 @@
-packages:=boost openssl zeromq cppzmq expat ldns libiconv hidapi protobuf libusb
+packages:=boost openssl zeromq cppzmq expat ldns readline libiconv hidapi protobuf libusb
 native_packages := native_ccache native_protobuf
 
 darwin_native_packages = native_biplist native_ds_store native_mac_alias
-darwin_packages = sodium-darwin ncurses readline 
+darwin_packages = sodium-darwin
 
-linux_packages = eudev ncurses readline unwind sodium
+linux_packages = eudev
 qt_packages = qt
 
 ifeq ($(build_tests),ON)
@@ -13,6 +13,11 @@ endif
 
 ifeq ($(host_os),mingw32)
 packages += icu4c
+packages += sodium
+endif
+
+ifeq ($(host_os),linux)
+packages += unwind
 packages += sodium
 endif
 
