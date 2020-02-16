808192f148a45349e9602b93a70fe6380e55e1de
Michal vel m@lbit <michal@arqma.com>
2020-02-16 00:28:06 +0000

    base_reward bug fix

--- src/blockchain_utilities/blockchain_ancestry.cpp
+++ src/blockchain_utilities/blockchain_ancestry.cpp
@@ -80,7 +80,7 @@ namespace std
 
 struct tx_data_t
 {
-  std::vector<std::pair<uint64_t, std::vector<uint64_t>>> vin;
+  std::vector<std::pair<uint64_t, std::vector<uint64_t> > > vin;
   std::vector<crypto::public_key> vout;
   bool coinbase;
 
