8ee8c90655ae2025b1ea521b1de1c88164345da0
muscleman <musclesonvacation@hotmail.com>
2020-02-26 21:07:48 -0600

    threading, inproc msg's, broker and dealer

--- src/arqma_mq/arqmaMQ.cpp
+++ src/arqma_mq/arqmaMQ.cpp
@@ -6,27 +6,118 @@ namespace arqmaMQ
         delete reinterpret_cast<std::string*>(hint);
     }
 
+    inline static int
+    s_send(void *socket, const char *string, int flags = 0) {
+        int rc;
+        zmq_msg_t message;
+        zmq_msg_init_size(&message, strlen(string));
+        memcpy(zmq_msg_data(&message), string, strlen(string));
+        rc = zmq_msg_send(&message, socket, flags);
+        assert(-1 != rc);
+        zmq_msg_close(&message);
+        return (rc);
+    }
+
+    inline static bool
+    s_send (zmq::socket_t & socket, const std::string & string, int flags = 0) {
+
+        zmq::message_t message(string.size());
+        memcpy (message.data(), string.data(), string.size());
+        bool rc = socket.send (message, flags);
+        return (rc);
+    }
+
+    inline static bool 
+    s_sendmore (zmq::socket_t & socket, const std::string & string) {
+        zmq::message_t message(string.size());
+        memcpy (message.data(), string.data(), string.size());
+        bool rc = socket.send (message, ZMQ_SNDMORE);
+        return (rc);
+    }
+
     ArqmaNotifier::ArqmaNotifier()
     {
-        socket.bind("tcp://127.0.0.1:3000");
+        producer.bind("inproc://backend");
+        proxy_thread = std::thread{&ArqmaNotifier::proxy_loop, this};
     }
 
     ArqmaNotifier::~ArqmaNotifier()
     {
-        zmq_close(&socket);
+        producer.send(create_message(std::move("QUIT")), 0);
+        proxy_thread.join();
+        zmq_close(&producer);
+        zmq_close(&subscriber);
+        zmq_term(&context);
     }
 
-    zmq::message_t ArqmaNotifier::create_message(std::string &&data) {
+    zmq::message_t ArqmaNotifier::create_message(std::string &&data)
+    {
         auto *buffer = new std::string(std::move(data));
         return zmq::message_t{&(*buffer)[0], buffer->size(), message_buffer_destroy, buffer};
     };
 
-    void ArqmaNotifier::notify(std::string &&data) {
-        std::cout << data << std::endl;
-        //auto *buffer = new std::string(std::move(data));
-        //zmq::message_t msg = zmq::message_t{&(*buffer)[0], buffer->size(), message_buffer_destroy, buffer};
-//        std::string id = new std::string("getblocktemplate");
-        socket.send(create_message(std::move("getblocktemplate")), ZMQ_SNDMORE);
-        socket.send(create_message(std::move(data)), 0);
+    void ArqmaNotifier::notify(std::string &&data)
+    {
+        //std::cout << data << std::endl;
+        producer.send(create_message(std::move("getblocktemplate")), ZMQ_SNDMORE);
+        producer.send(create_message(std::move(data)), 0);
+    }
+
+    void ArqmaNotifier::proxy_loop() 
+    {
+        subscriber.connect("inproc://backend");
+        listener.bind("tcp://127.0.0.1:3000");
+
+        zmq::pollitem_t items[2];
+        items[0].socket = (void*)subscriber;
+        items[0].fd = 0;
+        items[0].events = ZMQ_POLLIN;
+        items[1].socket = (void*)listener;
+        items[1].fd = 0;
+        items[1].events = ZMQ_POLLIN;
+
+        std::string id;
+        std::string quit("QUIT");
+
+        while (true) 
+        {
+            int rc = zmq::poll(items, 2, 1);
+            if (items[0].revents & ZMQ_POLLIN)
+            {
+                zmq::message_t envelope;
+                subscriber.recv(&envelope);
+                std::string stop = std::string(static_cast<char*>(envelope.data()), envelope.size());
+                if (stop == quit) 
+                {
+                    std::cout << "closing thread" << std::endl;
+                    break;
+                }
+                subscriber.recv(&envelope);
+                std::string identity = std::string(static_cast<char*>(envelope.data()), envelope.size());
+                std::cout << identity << std::endl;
+
+                //TODO: iterate list of <id, command>
+                if (!id.empty())
+                {
+                    s_sendmore(listener, id);
+                    s_send(listener, identity);
+                }
+
+            }
+
+            if (items[1].revents & ZMQ_POLLIN)
+            {
+                zmq::message_t envelope1;
+                listener.recv(&envelope1);
+                std::string msg = std::string(static_cast<char*>(envelope1.data()), envelope1.size());
+                //TODO: record <id, command>
+                id = std::move(msg);
+                listener.recv(&envelope1);
+                listener.recv(&envelope1);
+                std::string msg1 = std::string(static_cast<char*>(envelope1.data()), envelope1.size());
+                std::cout << "received " <<  id << " " << msg1 << std::endl;
+            }
+        }
+        zmq_close(&listener);
     }
 }
--- src/arqma_mq/arqmaMQ.h
+++ src/arqma_mq/arqmaMQ.h
@@ -2,10 +2,10 @@
 
 #include <iostream>
 #include <string>
+#include <thread>
 //#include <memory>
 #include <zmq.hpp>
 #include "INotifier.h"
-
 
 namespace arqmaMQ {
 
@@ -17,12 +17,15 @@ namespace arqmaMQ {
             ArqmaNotifier& operator=(const ArqmaNotifier&) = delete;
             ArqmaNotifier(ArqmaNotifier&&) = delete;
             ArqmaNotifier& operator=(ArqmaNotifier&&) = delete;
-          void notify(std::string &&data);
-
+            void notify(std::string &&data);
         private:
+            std::thread proxy_thread;
             zmq::context_t context;
-            zmq::socket_t socket{context, ZMQ_PUB};
+            zmq::socket_t listener{context, ZMQ_ROUTER};
+            zmq::socket_t producer{context, ZMQ_PAIR};
+            zmq::socket_t subscriber{context, ZMQ_PAIR};
             zmq::message_t create_message(std::string &&data);
+            void proxy_loop();
     };
 }
 
