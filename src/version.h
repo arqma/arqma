c5e75e0cb7fcd05770ba35da5b2bb5d4cc643e74
m@lbit <michal@arqma.com>
2020-03-04 01:15:15 +0000

    tweaks and typo fixes

--- src/version.h
+++ src/version.h
@@ -3,5 +3,6 @@
 extern const char* const ARQMA_VERSION_TAG;
 extern const char* const ARQMA_VERSION;
 extern const char* const ARQMA_RELEASE_NAME;
+extern const char* const ARQMA_PATCH;
 extern const char* const ARQMA_VERSION_FULL;
 extern const bool ARQMA_VERSION_IS_RELEASE;
