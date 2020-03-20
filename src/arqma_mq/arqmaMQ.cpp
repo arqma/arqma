8c857e7f6e2bda2c847d3a393d60df221015dc68
muscleman <musclesonvacation@hotmail.com>
2020-03-19 22:18:41 -0500

    Update arqmaMQ.cpp

--- src/arqma_mq/arqmaMQ.cpp
+++ src/arqma_mq/arqmaMQ.cpp
@@ -30,9 +30,6 @@
 // Parts of this file are originally copyright (c) 2012-2013 The Cryptonote developers
 
 #include "arqmaMQ.h"
-
-#include "arqmaMQ.h"
-
 #include <cstdint>
 #include <system_error>
 
