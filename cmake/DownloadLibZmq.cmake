8b0e2b7a1d5f65f98568c59689953d093165b88d
m@lbit <michal@arqma.com>
2020-06-18 08:07:05 +0000

    zmq windows patch

--- cmake/DownloadLibZmq.cmake
+++ cmake/DownloadLibZmq.cmake
@@ -3,9 +3,6 @@ set(ZeroMQ_VERSION 4.3.2)
 set(LIBZMQ_URL https://github.com/zeromq/libzmq/releases/download/v${ZeroMQ_VERSION}/zeromq-${ZeroMQ_VERSION}.tar.gz)
 set(LIBZMQ_HASH SHA512=b6251641e884181db9e6b0b705cced7ea4038d404bdae812ff47bdd0eed12510b6af6846b85cb96898e253ccbac71eca7fe588673300ddb9c3109c973250c8e4)
 
-set(LIBZMQ_GIT https://github.com/zeromq/libzmq.git)
-set(LIBZMQ_COMMIT b3123a2fd1e77cbdceb5ee7a70e796063b5ee5b9)
-
 if(LIBZMQ_TARBALL_URL)
     # make a build time override of the tarball url so we can fetch it if the original link goes away
     set(LIBZMQ_URL ${LIBZMQ_TARBALL_URL})
@@ -23,8 +20,7 @@ else()
   include(ExternalProject)
   include(ProcessorCount)
 
-  set(WIN32_FIX git checkout ${LIBZMQ_COMMIT})
-  set(ZeroMQ_AUTOCONF ./autogen.sh)
+  set(WIN_PATCH patch -p1 src/thread.cpp < ${PROJECT_SOURCE_DIR}/external/zmq.patch)
   set(ZeroMQ_CONFIGURE ./configure --prefix=${LIBZMQ_PREFIX})
 
   if(APPLE OR CMAKE_C_COMPILER_ID STREQUAL "AppleClang")
@@ -37,28 +33,17 @@ else()
     set(ZeroMQ_CONFIGURE ${ZeroMQ_CONFIGURE} --with-pic)
   endif()
 
-  if(WIN32 OR MSVC OR MINGW)
-    ExternalProject_Add(libzmq_external
-      BUILD_IN_SOURCE ON
-      PREFIX ${LIBZMQ_PREFIX}
-      GIT_REPOSITORY ${LIBZMQ_GIT}
-      CONFIGURE_COMMAND ${WIN32_FIX} && ${ZeroMQ_AUTOCONF} && ${ZeroMQ_CONFIGURE}
-      BUILD_COMMAND make -j${PROCESSOR_COUNT}
-      INSTALL_COMMAND ${MAKE}
-      BUILD_BYPRODUCTS ${LIBZMQ_PREFIX}/lib/libzmq.a ${LIBZMQ_PREFIX}/include
-    )
-  else()
-    ExternalProject_Add(libzmq_external
+  ExternalProject_Add(libzmq_external
       BUILD_IN_SOURCE ON
       PREFIX ${LIBZMQ_PREFIX}
       URL ${LIBZMQ_URL}
       URL_HASH ${LIBZMQ_HASH}
-      CONFIGURE_COMMAND ${ZeroMQ_AUTOCONF} && ${ZeroMQ_CONFIGURE}
+      PATCH_COMMAND ${WIN_PATCH}
+      CONFIGURE_COMMAND ${ZeroMQ_CONFIGURE}
       BUILD_COMMAND make -j${PROCESSOR_COUNT}
       INSTALL_COMMAND ${MAKE}
       BUILD_BYPRODUCTS ${LIBZMQ_PREFIX}/lib/libzmq.a ${LIBZMQ_PREFIX}/include
-    )
-  endif()
+  )
 
   add_library(lib-zmq STATIC IMPORTED GLOBAL)
   add_dependencies(libzmq libzmq_external)
