58ec2f4cd58d01719f3b4031f502cd5d934b94f3
m@lbit <michal@arqma.com>
2020-06-22 07:20:46 +0000

    sdk deletion

--- contrib/depends/SKDs/MacOSX10.15.sdk/System/Library/Frameworks/JavaVM.framework/Versions/A/Frameworks/JavaRuntimeSupport.framework/Versions/A/Headers/JRSProperties.h
+++
@@ -1,8 +0,0 @@
-//
-//  JRSProperties.h
-//  Copyright 2008-2011 Apple Inc. All rights reserved.
-//
-
-char *JRSCopyOSName(void);
-char *JRSCopyOSVersion(void);
-char *JRSCopyOSJavaSupportVersion(void);
