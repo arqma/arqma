c03782caf25886ae1388cb913556b54dc294aece
Michal vel m@lbit <michal@arqma.com>
2020-06-29 22:18:41 +0100

    this should be here

--- src/blockchain_utilities/blockchain_usage.cpp
+++ src/blockchain_utilities/blockchain_usage.cpp
@@ -149,6 +149,7 @@ int main(int argc, char* argv[])
   // because unlike blockchain_storage constructor, which takes a pointer to
   // tx_memory_pool, Blockchain's constructor takes tx_memory_pool object.
   LOG_PRINT_L0("Initializing source blockchain (BlockchainDB)");
+  const std::string input = command_line::get_arg(vm, arg_input);
   blockchain_objects_t blockchain_objects = {};
   Blockchain *core_storage = &blockchain_objects.m_blockchain;
   BlockchainDB *db = new_db();
