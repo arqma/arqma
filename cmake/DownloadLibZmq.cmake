4a0e21311657eb3cfdb377a8e02b8696280a9505
m@lbit <michal@arqma.com>
2020-10-15 03:37:41 +0100

    remodeling CMake generator. build detection moved way sooner. ccache is by default OFF at MINGW (compilation was freezing at 6% with no cpu/memory/disc usage while power_usage was max). ZMQ + unbound + miniupnpc are always in-tree built (no system libs involved).

--- cmake/DownloadLibZmq.cmake
+++ cmake/DownloadLibZmq.cmake
@@ -4,15 +4,14 @@ set(LIBZMQ_URL https://github.com/zeromq/libzmq/releases/download/v${ZeroMQ_VERS
 set(LIBZMQ_HASH SHA512=b6251641e884181db9e6b0b705cced7ea4038d404bdae812ff47bdd0eed12510b6af6846b85cb96898e253ccbac71eca7fe588673300ddb9c3109c973250c8e4)
 
 if(LIBZMQ_TARBALL_URL)
-    # make a build time override of the tarball url so we can fetch it if the original link goes away
-    set(LIBZMQ_URL ${LIBZMQ_TARBALL_URL})
+  # make a build time override of the tarball url so we can fetch it if the original link goes away
+  set(LIBZMQ_URL ${LIBZMQ_TARBALL_URL})
 endif()
 
 if(DEPENDS)
-  add_library(lib-zmq STATIC IMPORTED GLOBAL)
-  set_target_properties(lib-zmq PROPERTIES IMPORTED_LOCATION ${ZMQ_LIBRARIES})
-  target_link_libraries(lib-zmq INTERFACE sodium)
-  target_include_directories(lib-zmq INTERFACE ${ZMQ_INCLUDE_DIRS})
+  add_library(libzmq_vendor STATIC IMPORTED GLOBAL)
+  set_target_properties(libzmq_vendor PROPERTIES IMPORTED_LOCATION ${ZMQ_LIBRARIES})
+  target_include_directories(libzmq_vendor INTERFACE ${ZMQ_INCLUDE_DIRS})
   message(STATUS "${ZMQ_INCLUDE_DIRS} ${ZMQ_LIBRARIES}")
 else()
   file(MAKE_DIRECTORY ${LIBZMQ_PREFIX}/include)
@@ -20,10 +19,10 @@ else()
   include(ExternalProject)
   include(ProcessorCount)
 
-  set(WIN_PATCH patch -p1 src/thread.cpp < ${PROJECT_SOURCE_DIR}/external/zmq.patch)
+  set(ZeroMQ_PATCH patch -p1 src/thread.cpp < ${PROJECT_SOURCE_DIR}/utils/patches/zmq.patch)
   set(ZeroMQ_CONFIGURE ./configure --prefix=${LIBZMQ_PREFIX})
 
-  if(APPLE OR CMAKE_C_COMPILER_ID STREQUAL "AppleClang")
+  if(APPLE OR CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang")
     set(ZeroMQ_CONFIGURE ${ZeroMQ_CONFIGURE} --host=x86_64-apple-darwin)
   endif()
 
@@ -38,21 +37,20 @@ else()
       PREFIX ${LIBZMQ_PREFIX}
       URL ${LIBZMQ_URL}
       URL_HASH ${LIBZMQ_HASH}
-      PATCH_COMMAND ${WIN_PATCH}
+      PATCH_COMMAND ${ZeroMQ_PATCH}
       CONFIGURE_COMMAND ${ZeroMQ_CONFIGURE}
       BUILD_COMMAND make -j${PROCESSOR_COUNT}
       INSTALL_COMMAND ${MAKE}
       BUILD_BYPRODUCTS ${LIBZMQ_PREFIX}/lib/libzmq.a ${LIBZMQ_PREFIX}/include
   )
 
-  add_library(lib-zmq STATIC IMPORTED GLOBAL)
-  add_dependencies(libzmq libzmq_external)
-  target_link_libraries(lib-zmq INTERFACE sodium)
-  set_property(TARGET lib-zmq PROPERTY IMPORTED_LOCATION ${LIBZMQ_PREFIX}/lib/libzmq.a)
-  target_include_directories(lib-zmq INTERFACE ${LIBZMQ_PREFIX}/include)
+  add_library(libzmq_vendor STATIC IMPORTED GLOBAL)
+  add_dependencies(libzmq_vendor libzmq_external)
+  set_property(TARGET libzmq_vendor PROPERTY IMPORTED_LOCATION ${LIBZMQ_PREFIX}/lib/libzmq.a)
+  target_include_directories(libzmq_vendor INTERFACE ${LIBZMQ_PREFIX}/include)
 
   set(ZMQ_INCLUDE_DIRS ${LIBZMQ_PREFIX}/include CACHE STRING "ZMQ Include path")
 
-  link_dep_libs(lib-zmq INTERFACE "${ZMQ_LIBRARY_DIRS}" ${LIBZMQ_PREFIX}/lib/libzmq.a)
-  target_include_directories(lib-zmq INTERFACE ${ZMQ_INCLUDE_DIRS})
+  link_dep_libs(libzmq_vendor INTERFACE "${ZMQ_LIBRARY_DIRS}" ${LIBZMQ_PREFIX}/lib/libzmq.a)
+  target_include_directories(libzmq_vendor INTERFACE ${ZMQ_INCLUDE_DIRS})
 endif()
