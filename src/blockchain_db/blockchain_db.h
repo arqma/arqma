c5b354c337c6801b3c001407e7aab5b8c1c5861c
m@lbit <michal@arqma.com>
2021-01-13 05:53:31 +0000

    SN Database Storing Fixes.
    Co-authored-by: tewinget <tewinget@gmail.com>

--- src/blockchain_db/blockchain_db.h
+++ src/blockchain_db/blockchain_db.h
@@ -1822,6 +1822,10 @@ public:
    */
   virtual void fixup();
 
+  virtual void set_service_node_data(const std::string &data) = 0;
+  virtual void get_service_node_data(std::string &data) = 0;
+  virtual void clear_service_node_data() = 0;
+
   /**
    * @brief set whether or not to automatically remove logs
    *
