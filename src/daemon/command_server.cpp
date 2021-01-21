3d8c3f3582f57da4f872f5fde37c1eb1ee5a2e17
m@lbit <michal@arqma.com>
2021-01-21 00:47:28 +0000

    removed get_service_node_registration from client cmd + print_sn_status cmd including print_sn and sn_key informations

--- src/daemon/command_server.cpp
+++ src/daemon/command_server.cpp
@@ -109,12 +109,6 @@ t_command_server::t_command_server(
     , "Print the quorum state at given block height."
     );
   m_command_lookup.set_handler(
-      "get_service_node_registration_cmd"
-    , std::bind(&t_command_parser_executor::get_service_node_registration_cmd, &m_parser, p::_1)
-    , "get_service_node_registration_cmd <address1> <shares1> [<address2> <shares2> [...]]"
-    , "Generate the required registration command"
-    );
-  m_command_lookup.set_handler(
       "print_sn_key"
     , std::bind(&t_command_parser_executor::print_sn_key, &m_parser, p::_1)
     , "print_sn_key"
@@ -131,6 +125,12 @@ t_command_server::t_command_server(
     , std::bind(&t_command_parser_executor::print_sn, &m_parser, p::_1)
     , "print_sn [<pubkey> [...]]"
     , "Print Service_node registration info for the current height"
+    );
+  m_command_lookup.set_handler(
+      "print_sn_status"
+    , std::bind(&t_command_parser_executor::print_sn_status, &m_parser, p::_1)
+    , "print_sn_status"
+    , "Print Service Node registration info for this Service Node"
     );
   m_command_lookup.set_handler(
       "is_key_image_spent"
