39ce7737a017694e79d7af35d8516b88801f0179
BKdilse <Bharat.Khetani@gntl.co.uk>
2021-02-27 10:48:38 +0000

    Max supply increase to create Devil amount :)

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -50,7 +50,7 @@
 #define BLOCKCHAIN_TIMESTAMP_CHECK_WINDOW_V9            BLOCKCHAIN_TIMESTAMP_CHECK_WINDOW_V2
 
 // MONEY_SUPPLY - total number coins to be generated
-#define MONEY_SUPPLY                                    ((uint64_t)50000000000000000)
+#define MONEY_SUPPLY                                    ((uint64_t)77700000000000000)
 #define MONEY_PREMINE                                   ((uint64_t)30)
 #define EMISSION_SPEED_FACTOR_PER_MINUTE                (21)
 #define FINAL_SUBSIDY_PER_MINUTE                        ((uint64_t)300000000)
