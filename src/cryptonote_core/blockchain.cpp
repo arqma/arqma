bbe66ae349acf008ccc2f01ba34cee6b20cf1ffb
malbit <michal@arqma.com>
2022-07-28 16:13:30 -0500

    hardcoded checkpoints update

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -5138,7 +5138,7 @@ void Blockchain::cancel()
 }
 
 #if defined(PER_BLOCK_CHECKPOINT)
-static const char expected_block_hashes_hash[] = "5e301ca6aca3ae52a88037164b553d321391e10062b9aa6c9e1c7713f3ae9eee";
+static const char expected_block_hashes_hash[] = "8d5de2be461412c39c187edb3efec9e27e67b1a9b4f1456f7433f314709742e1";
 void Blockchain::load_compiled_in_block_hashes(const GetCheckpointsCallback& get_checkpoints)
 {
   if (get_checkpoints == nullptr || !m_fast_sync)
