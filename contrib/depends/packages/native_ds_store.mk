8a7574946d0a0163275ed08d3513fa530df5cb9a
m@lbit <michal@arqma.com>
2023-01-30 21:26:59 +0000

    depends packages sha256 sums update

--- contrib/depends/packages/native_ds_store.mk
+++ contrib/depends/packages/native_ds_store.mk
@@ -2,7 +2,7 @@ package=native_ds_store
 $(package)_version=1.3.0
 $(package)_download_path=https://github.com/al45tair/ds_store/archive/
 $(package)_file_name=v$($(package)_version).tar.gz
-$(package)_sha256_hash=76b3280cd4e19e5179defa23fb594a9dd32643b0c80d774bd3108361d94fb46d
+$(package)_sha256_hash=78999ca6fd6a9b2b08a5dc04503d98d7a4337ada80132856c1fb963608841ca0
 $(package)_install_libdir=$(build_prefix)/lib/python3/dist-packages
 $(package)_dependencies=native_biplist
 
--- contrib/depends/packages/native_mac_alias.mk
+++ contrib/depends/packages/native_mac_alias.mk
@@ -3,7 +3,7 @@ $(package)_version=2.2.0
 $(package)_download_path=https://github.com/al45tair/mac_alias/archive/
 $(package)_download_file=v$($(package)_version).tar.gz
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=421e6d7586d1f155c7db3e7da01ca0dacc9649a509a253ad7077b70174426499
+$(package)_sha256_hash=a853678621bab213fee1b338d351da9d53d1a20b9ad55da35b1129dcaca6c9df
 $(package)_install_libdir=$(build_prefix)/lib/python3/dist-packages
 
 define $(package)_build_cmds
