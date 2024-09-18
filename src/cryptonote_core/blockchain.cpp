2f267355b4292a1a8c9179fdd233e299744f7dcb
ArqTras <biuro@ar-net.info>
2024-09-18 21:41:42 +0200

    checkpoints

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -5138,7 +5138,7 @@ void Blockchain::cancel()
 }
 
 #if defined(PER_BLOCK_CHECKPOINT)
-static const char expected_block_hashes_hash[] = "8d5de2be461412c39c187edb3efec9e27e67b1a9b4f1456f7433f314709742e1";
+static const char expected_block_hashes_hash[] = "94f30f330d8dee7f89929088bf7ad182f62d358a7f367b35fa67e893a7249e7d";
 void Blockchain::load_compiled_in_block_hashes(const GetCheckpointsCallback& get_checkpoints)
 {
   if (get_checkpoints == nullptr || !m_fast_sync)
