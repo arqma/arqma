03424a3992fb47bf0543824a1109598302ae207c
Michal m@lbit Malicki <michal@arqma.com>
2025-11-26 22:51:49 +0100

    need to be removed when hardfork will be set

--- src/cryptonote_core/service_node_rules.h
+++ src/cryptonote_core/service_node_rules.h
@@ -78,7 +78,7 @@ namespace service_nodes
   };
 
   constexpr proof_version MIN_UPTIME_PROOF_VERSIONS[] = {
-    {cryptonote::network_version_18, {9,0,0}},
+    //{cryptonote::network_version_18, {9,0,0}},
     {cryptonote::network_version_17, {8,0,0}},
     {cryptonote::network_version_16, {7,2,0}},
   };
